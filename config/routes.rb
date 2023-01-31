2dc1cbae4f9f4cad4ad241223564b45696609d70
Jason Duckworth <duckworthj39@gmail.com>
2023-01-31 11:07:37 +0000

    Added functionality to choose between security or gov rulesets

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  # post '/linter', to: "linter#new"
+  resources :linter, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
