be9335313d8550a762614dfcf383289753bf7f62
Darryl Ryan <darryl.ryan@tpximpact.com>
2023-02-01 16:45:22 +0000

    xit the test

--- spec/lib/linters/crunch_api_spec.rb
+++ spec/lib/linters/crunch_api_spec.rb
@@ -5,7 +5,7 @@ describe CrunchApi do
 
   # Remove the x from xit and put the .json file in the root of the project
   # if you want to test the output
-  it "just outputs the linter results as a string" do
+  xit "just outputs the linter results as a string" do
     crunch = CrunchApi.new
     puts crunch.lint('ons-vat.json')
   end
