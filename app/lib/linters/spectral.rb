1238d0a2e75559ce565a8ba2e65c174c25ecfa2c
Jason Duckworth <duckworthj39@gmail.com>
2023-02-07 16:50:46 +0000

    improved spectral class coupling

--- app/lib/linters/spectral.rb
+++ app/lib/linters/spectral.rb
@@ -2,12 +2,13 @@ module Linters
   class SpectralLinterError < StandardError; end
   class Spectral
 
-    def initialize(file:)
+    def initialize(file:, system: Open3)
       @file = file
+      @system = system
     end
 
     def lint_to_json
-      stdout_str, stderr_str = Open3.capture3("npx spectral lint -f json #{file.path}")
+      stdout_str, stderr_str = system.capture3("npx spectral lint -f json #{file.path}")
       raise_error(stderr_str) if stderr_str.present?
 
       stdout_str
@@ -15,7 +16,7 @@ module Linters
 
     private
 
-    attr_reader :file
+    attr_reader :file, :system
 
     def raise_error(error_message)
       Rails.logger.error("npx spectral command failed: #{error_message}")
--- spec/lib/linters/spectral_spec.rb
+++ spec/lib/linters/spectral_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 describe Linters::Spectral do
   let(:file) { instance_double(File, path: '/tmp/file.txt') }
-  subject { described_class.new(file:) }
+  let(:system) { class_spy(Open3) }
+  subject { described_class.new(file:, system: system) }
 
   context 'when the file is a valid json file' do
     it 'executes the spectral lint command' do
-      expect(Open3).to receive(:capture3).with("npx spectral lint -f json /tmp/file.txt").and_return("{}")
+      expect(system).to receive(:capture3).with("npx spectral lint -f json /tmp/file.txt").and_return("{}")
       output = subject.lint_to_json
       expect(output).to eq("{}")
       expect(file).to have_received(:path)
@@ -15,7 +16,7 @@ describe Linters::Spectral do
 
   context 'When the command fails' do
     it 'raises an error' do
-      expect(Open3).to receive(:capture3).with("npx spectral lint -f json /tmp/file.txt").and_raise(Linters::SpectralLinterError)
+      expect(system).to receive(:capture3).with("npx spectral lint -f json /tmp/file.txt").and_raise(Linters::SpectralLinterError)
       expect { subject.lint_to_json }.to raise_error(Linters::SpectralLinterError)
     end
   end
