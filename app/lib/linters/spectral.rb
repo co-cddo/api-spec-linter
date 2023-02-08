9d3254fc3688fc0053985123493dd0c44f284f29
Jason Duckworth <duckworthj39@gmail.com>
2023-02-08 08:32:18 +0000

    improved use of error handling for spectral

--- app/lib/linters/spectral.rb
+++ app/lib/linters/spectral.rb
@@ -2,26 +2,23 @@ module Linters
   class SpectralLinterError < StandardError; end
   class Spectral
 
-    def initialize(file:, system: Open3)
+    def initialize(file:, system_command: Open3)
       @file = file
-      @system = system
+      @system_command = system_command
     end
 
     def lint_to_json
-      stdout_str, stderr_str = system.capture3("npx spectral lint -f json #{file.path}")
-      raise_error(stderr_str) if stderr_str.present?
 
+      stdout_str, stderr_str = system_command.capture3("npx spectral lint -f json #{file.path}")
+      raise SpectralLinterError if stderr_str.present?
       stdout_str
+    rescue StandardError => e
+      Rails.logger.error("npx spectral command failed: #{e.message}")
+      raise SpectralLinterError, "npx spectral command failed"
     end
 
     private
 
-    attr_reader :file, :system
-
-    def raise_error(error_message)
-      Rails.logger.error("npx spectral command failed: #{error_message}")
-
-      raise SpectralLinterError, "npx spectral command failed"
-    end
+    attr_reader :file, :system_command
   end
 end
--- spec/lib/linters/spectral_spec.rb
+++ spec/lib/linters/spectral_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 describe Linters::Spectral do
-  let(:file) { instance_double(File, path: '/tmp/file.txt') }
-  let(:system) { class_spy(Open3) }
-  subject { described_class.new(file:, system: system) }
+  let(:file) { instance_double(File, path: '/tmp/file.json') }
+  let(:system_command) { class_spy(Open3) }
+  subject { described_class.new(file: file, system_command: system_command) }
 
-  context 'when the file is a valid json file' do
+  context 'When the command successfully runs' do
     it 'executes the spectral lint command' do
-      expect(system).to receive(:capture3).with("npx spectral lint -f json /tmp/file.txt").and_return("{}")
+      expect(system_command).to receive(:capture3).with("npx spectral lint -f json /tmp/file.json").and_return("{}")
       output = subject.lint_to_json
       expect(output).to eq("{}")
       expect(file).to have_received(:path)
@@ -16,7 +16,7 @@ describe Linters::Spectral do
 
   context 'When the command fails' do
     it 'raises an error' do
-      expect(system).to receive(:capture3).with("npx spectral lint -f json /tmp/file.txt").and_raise(Linters::SpectralLinterError)
+      expect(system_command).to receive(:capture3).with("npx spectral lint -f json /tmp/file.json").and_raise(Linters::SpectralLinterError)
       expect { subject.lint_to_json }.to raise_error(Linters::SpectralLinterError)
     end
   end
