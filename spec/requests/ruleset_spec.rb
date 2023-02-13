74f5e7b008422cc03783d73be3775f080e3f4b9c
Jason Duckworth <duckworthj39@gmail.com>
2023-02-13 15:33:59 +0000

    Changed which_ruleset to ruleset_name

--- spec/requests/ruleset_spec.rb
+++ spec/requests/ruleset_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 describe "RulesetController", type: :request do
   describe "GET /ruleset/new" do
     it "renders a page with a form for the security ruleset" do
-      get new_ruleset_path(which_ruleset: "security_ruleset")
+      get new_ruleset_path(ruleset_name: "security_ruleset")
 
       expect(response).to render_template("security_ruleset")
     end
@@ -13,7 +13,7 @@ describe "RulesetController", type: :request do
 
   describe "POST /ruleset" do
     it "returns a 200" do
-      post("/ruleset", params: { which_ruleset: "security_ruleset" })
+      post("/ruleset", params: { ruleset_name: "security_ruleset" })
 
       expect(response).to have_http_status(:ok)
     end
