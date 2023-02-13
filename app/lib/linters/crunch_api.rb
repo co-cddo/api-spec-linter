c134b645ffa7c14bfc8e91ef63c062df9a28ed3c
Darryl Ryan <darryl.ryan@tpximpact.com>
2023-02-13 10:55:49 +0000

    Rubocopped

--- app/lib/linters/crunch_api.rb
+++ app/lib/linters/crunch_api.rb
@@ -14,7 +14,8 @@ module Linters
     # Post the file to 42Crunch according to their API v1 "Create an API (from file)" spec
     # https://www.postman.com/get-42crunch/workspace/42crunch-api/request/13761657-335bcb36-c68f-43a1-823d-741b62b55bc6
     def create_api
-    result = RestClient.post(URI.join(@base_url, "/api/v1/apis").to_s,
+
+      RestClient.post(URI.join(@base_url, "/api/v1/apis").to_s,
       { # Body of the request
         cid: ENV['COLLECTION_ID'], # Collection id, returned by "Create a collection"
         name: File.basename(file.path, ".*"), # API Display Name
@@ -25,7 +26,6 @@ module Linters
       'X-API-KEY': ENV['CRUNCH_API_KEY']
       })
 
-      return result
     end
 
     # Use the created API ID to get the linting results from 42Crunch using their
@@ -34,14 +34,11 @@ module Linters
     # @param id [String] the ID of the report to retrieve
     # @return [String] the raw JSON response
     def retrieve_report(id)
-      url = URI.join(@base_url, "/api/v1/apis/", id + "/", "assessmentreport").to_s
-      puts "Retrieving #{url}"
-      result = RestClient.get(url,
+      url = URI.join(@base_url, "/api/v1/apis/", "#{id}/", "assessmentreport").to_s
+      RestClient.get(url,
       { # Request headers
       'X-API-KEY': ENV['CRUNCH_API_KEY']
       })
-
-      return result
     end
 
     # Lints an unploaded file with 42crunch.
@@ -58,14 +55,12 @@ module Linters
       # If we try to get the report too quickly 42crunch will return a 404 error
       # So we need to use exponential backoff from the 'retries' gem
       report_response = ""
-      with_retries(:max_tries => 10, :rescue => [RestClient::RequestFailed]) do
+      with_retries(max_tries: 10, rescue: [RestClient::RequestFailed]) do
         report_response = retrieve_report(created_api_id)
       end
 
       # Parse the JSON response to get the report data
-      decoded_report_data = Base64.decode64(JSON.parse(report_response)["data"])
-
-      return decoded_report_data
+      Base64.decode64(JSON.parse(report_response)["data"])
 
     end
 
