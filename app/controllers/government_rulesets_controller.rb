2fd46c77e555f57ed2ea049bca66bb9490b25208
Darryl Ryan <darryl.ryan@tpximpact.com>
2023-02-27 19:09:38 +0000

    Refactor file writer to the service

--- app/controllers/government_rulesets_controller.rb
+++ app/controllers/government_rulesets_controller.rb
@@ -5,13 +5,12 @@ class GovernmentRulesetsController < ApplicationController
   before_action :clear_errors
 
   def show
-    oas_file_body = session[:oas_file_body]
-    oas_file_name = session[:oas_file_name]
-    Tempfile.open(oas_file_name) do |oas_file|
-      oas_file.write(oas_file_body)
-      oas_file.rewind
-      @spectral_results = JSON.parse(Linters::Spectral.new(file: oas_file).lint_to_json)
-    end
+    @spectral_results = JSON.parse(
+      Linters::Spectral.new(
+        file_name: session[:oas_file_name],
+        file_body: session[:oas_file_body]
+        ).lint_to_json
+    )
   end
 
   private
--- app/lib/linters/spectral.rb
+++ app/lib/linters/spectral.rb
@@ -3,23 +3,28 @@ module Linters
   class SpectralLinterError < StandardError; end
   class Spectral
 
-    def initialize(file:, system_command: Open3)
-      @file = file
+    def initialize(file_name:, file_body:, system_command: Open3)
+      @file_name = file_name
+      @file_body = file_body
       @system_command = system_command
     end
 
     def lint_to_json
-      stdout_str, stderr_str = system_command.capture3("npx spectral lint -f json #{file.path}")
-      raise SpectralLinterError stderr_str if stderr_str.present?
-      stdout_str
-    rescue StandardError => e
-      Rails.logger.error("npx spectral command failed: #{stderr_str}")
-      Rails.logger.error(stdout_str)
-      raise SpectralLinterError, "npx spectral command failed: #{e.message}"
+      Tempfile.open(@file_name) do |oas_file|
+        oas_file.write(@file_body)
+        oas_file.rewind
+        stdout_str, stderr_str = system_command.capture3("npx spectral lint -f json #{oas_file.path}")
+        raise SpectralLinterError stderr_str if stderr_str.present?
+        stdout_str
+        rescue StandardError => e
+          Rails.logger.error("npx spectral command failed: #{stderr_str}")
+          Rails.logger.error(stdout_str)
+          raise SpectralLinterError, "npx spectral command failed: #{e.message}"
+      end
     end
 
     private
 
-    attr_reader :file, :system_command
+    attr_reader :file_name, :file_body, :system_command
   end
 end
