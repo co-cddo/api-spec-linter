f7e04f8232f09dce93074055329a3b141212cfca
Darryl Ryan <darryl.ryan@tpximpact.com>
2023-02-28 14:27:19 +0000

    Moved max file size to a constant

--- app/controllers/concerns/upload_validations.rb
+++ app/controllers/concerns/upload_validations.rb
@@ -1,7 +1,14 @@
+# frozen_string_literal: true
 module UploadValidations
   extend ActiveSupport::Concern
 
   private
+
+  # Sets the maximum file size allowed as a constant
+  # If no value is specified in the environment variables we
+  # fall back to 100mb as a hard-coded safety limit
+  # Note: The web server configuration may enforce a lower limit
+  MAX_FILE_SIZE = Integer(ENV["MAX_FILE_SIZE"] || 100_000)
 
   def validate_file_upload!
 
@@ -15,12 +22,11 @@ module UploadValidations
     # If no value is specified in the environment variables we
     # fall back to 100mb as a hard-coded safety limit
     # Note: The web server configuration may enforce a lower limit
-    max = Integer(ENV["MAX_FILE_SIZE"] || 100000)
-    if params[:oas_file].size > max.kilobytes
+    if params[:oas_file].size > MAX_FILE_SIZE.kilobytes
       # We log this attempt so that we can see if lots of people are hitting the limit
       logger = Rails.logger
       logger.info "A user tried to upload a file of #{params[:oas_file].size.kilobytes} kilobytes"
-      flash[:error] = "Your file exceeds the maximum size of #{max} kilobytes"
+      flash[:error] = "Your file exceeds the maximum size of #{MAX_FILE_SIZE} kilobytes"
       render 'index'
     end
 
@@ -31,5 +37,4 @@ module UploadValidations
     end
 
   end
-
 end
