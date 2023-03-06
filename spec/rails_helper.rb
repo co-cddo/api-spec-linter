b253ad80c9126a12e5db6febde9f142b167f10a5
Darryl Ryan <darryl.ryan@tpximpact.com>
2023-03-06 11:38:45 +0000

    Update cassette

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,8 +65,5 @@ RSpec.configure do |config|
     config.hook_into :webmock
     config.ignore_localhost = true
     config.configure_rspec_metadata!
-    config.ignore_request do |request|
-      request.method == :delete
-    end
   end
 end
