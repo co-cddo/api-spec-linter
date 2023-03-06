43f2ff0182edbeb70004f1eb67614e05def00845
Darryl Ryan <darryl.ryan@tpximpact.com>
2023-03-06 11:17:23 +0000

    Configure VCR to ignore DELETE since we don't need a response anyway

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,5 +65,8 @@ RSpec.configure do |config|
     config.hook_into :webmock
     config.ignore_localhost = true
     config.configure_rspec_metadata!
+    config.ignore_request do |request|
+      request.method == :delete
+    end
   end
 end
