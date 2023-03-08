86a9d76f7ed804886fadd7539593d192ff71d9c2
Rob Nichols <robert.nichols@digital.cabinet-office.gov.uk>
2023-03-08 12:18:23 +0000

    Add local store of issue rule details with controller to view
    With this change in place the details links to:
    - the local rule path if the details are defined in the rules locale
    - a remote site which lists standard spectral rule details

---
+++ spec/requests/rules_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "Rules", type: :request do
+
+
+  describe "GET rules/:id" do
+    let(:rule) { 'oas3-strict' }
+    let(:translation) { I18n.t("rules.#{rule}") }
+    let(:output) do
+      doc = Govspeak::Document.new(translation)
+      doc.to_html.html_safe
+    end
+
+    it "returns http success" do
+      get rule_path(rule)
+      expect(response).to have_http_status(:success)
+    end
+
+    it "outputs the matching translation as markdown" do
+      get rule_path('oas3-strict')
+      expect(response.body).to include(output)
+    end
+  end
+
+end
