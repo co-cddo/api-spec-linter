49c4c084a7b766e8f51518405059bfe2178cd8dd
Jason Duckworth <duckworthj39@gmail.com>
2023-03-10 10:37:57 +0000

    added descriptions for ruleset radio buttons

--- app/lib/linters/rulesets.rb
+++ app/lib/linters/rulesets.rb
@@ -2,6 +2,6 @@
 
 module Linters
   module Rulesets
-    ACTIVE_RULESETS = %w[ruleset_low ruleset_medium ruleset_high].freeze
+    ACTIVE_RULESETS = %w[ruleset_low ruleset_medium ruleset_high security_ruleset].freeze
   end
 end
--- spec/views/rulesets/index_spec.rb
+++ spec/views/rulesets/index_spec.rb
@@ -5,13 +5,14 @@ RSpec.describe "rulesets/index.html.erb", type: :view do
   end
 
   it "displays the page title" do
-    expect(rendered).to have_selector("h1.govuk-heading-xl", text: "GOV.UK OpenAPI Linting Service")
+    assert_select("h1.govuk-heading-xl", text: "GOV.UK OpenAPI Linting Service")
   end
 
   it "displays the ruleset options" do
     assert_select("input[type=radio][name=ruleset_name][value=ruleset_low]")
     assert_select("input[type=radio][name=ruleset_name][value=ruleset_medium][checked]")
     assert_select("input[type=radio][name=ruleset_name][value=ruleset_high]")
+    assert_select("input[type=radio][name=ruleset_name][value=security_ruleset]")
     assert_select("input[type=submit][value=Continue]")
   end
 end
